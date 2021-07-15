9e999ca82b6dde0f39e44c3dd73e31f293fcf3f1
Hasan Kaplan <haskap84@gmail.com>
2021-07-15 10:05:17 +0300

    yeni event yapısı için değişiklikler yapılıyor.

--- Haskap.WorkflowEngine.Infrastructure.Data.NpgsqlDbContext/WorkflowEngineEfCoreNpgsqlDbContext.cs
+++ Haskap.WorkflowEngine.Infrastructure.Data.NpgsqlDbContext/WorkflowEngineEfCoreNpgsqlDbContext.cs
@@ -5,7 +5,6 @@ using Haskap.WorkflowEngine.Domain.Core.StateAggregate;
 using Haskap.WorkflowEngine.Domain.Core.UserAggregate;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Haskap.DddBase.Infrastructure.Mediator;
 using System;
 using System.Threading.Tasks;
 using System.Threading;
@@ -14,19 +13,15 @@ namespace Haskap.WorkflowEngine.Infrastructure.Data.NpgsqlDbContext
 {
     public class WorkflowEngineEfCoreNpgsqlDbContext : BaseEfCoreNpgsqlDbContext
     {
-        private readonly IMediator mediator;
-
         public DbSet<State> State { get; set; }
         public DbSet<User> User { get; set; }
         public DbSet<Process> Process { get; set; }
         public DbSet<Request> Request { get; set; }
 
         public WorkflowEngineEfCoreNpgsqlDbContext(
-            DbContextOptions<WorkflowEngineEfCoreNpgsqlDbContext> options,
-            IMediator mediator)
+            DbContextOptions<WorkflowEngineEfCoreNpgsqlDbContext> options)
             : base(options)
         {
-            this.mediator = mediator;
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -34,18 +29,6 @@ namespace Haskap.WorkflowEngine.Infrastructure.Data.NpgsqlDbContext
             builder.ApplyConfigurationsFromAssembly(typeof(WorkflowEngineEfCoreNpgsqlDbContext).Assembly);
 
             base.OnModelCreating(builder);
-        }
-
-        public override int SaveChanges()
-        {
-            mediator.DispatchDomainEventsAsync<WorkflowEngineEfCoreNpgsqlDbContext, Guid>(this).GetAwaiter().GetResult();
-            return base.SaveChanges();
-        }
-
-        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-        {
-            await mediator.DispatchDomainEventsAsync<WorkflowEngineEfCoreNpgsqlDbContext, Guid>(this, cancellationToken);
-            return await base.SaveChangesAsync(cancellationToken);
         }
     }
 }
