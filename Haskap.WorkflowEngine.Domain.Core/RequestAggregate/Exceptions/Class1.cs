1eaaf0bc8a5e9e9c00e40fc2d302e76a6123b837
Hasan Kaplan <hasan.kaplan@atilim.edu.tr>
2021-09-20 09:16:46 +0300

    Aggregate içindeki klasörlerin github içerisinde görünmesi için boş class' lar eklendi.

---
+++ Haskap.WorkflowEngine.Domain.Core/RequestAggregate/Exceptions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haskap.WorkflowEngine.Domain.Core.RequestAggregate.Exceptions
+{
+    class Class1
+    {
+    }
+}
